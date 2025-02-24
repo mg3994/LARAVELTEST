31dc29da0a8881684703e2f8649a1520820539de
mg3994 <manishgautammg7@yahoo.com>
2025-02-24 21:19:44 +0530

    WorkOs and Livewire with Flux

---
+++ resources/views/livewire/settings/delete-user-form.blade.php
@@ -0,0 +1,54 @@
+<?php
+
+use App\Models\User;
+use Laravel\WorkOS\Http\Requests\AuthKitAccountDeletionRequest;
+use Livewire\Volt\Component;
+
+new class extends Component {
+    public string $password = '';
+
+    /**
+     * Delete the currently authenticated user.
+     */
+    public function deleteUser(AuthKitAccountDeletionRequest $request): void
+    {
+        $request->delete(
+            using: fn (User $user) => $user->delete()
+        );
+
+        $this->redirect('/', navigate: true);
+    }
+}; ?>
+
+<section class="mt-10 space-y-6">
+    <div class="relative mb-5">
+        <flux:heading>{{ __('Delete Account') }}</flux:heading>
+        <flux:subheading>{{ __('Delete your account and all of its resources') }}</flux:subheading>
+    </div>
+
+    <flux:modal.trigger name="confirm-user-deletion">
+        <flux:button variant="danger" x-data="" x-on:click.prevent="$dispatch('open-modal', 'confirm-user-deletion')">
+            {{ __('Delete Account') }}
+        </flux:button>
+    </flux:modal.trigger>
+
+    <flux:modal name="confirm-user-deletion" :show="$errors->isNotEmpty()" focusable class="max-w-lg">
+        <form wire:submit="deleteUser">
+            <h2 class="text-lg font-medium text-gray-900">
+                {{ __('Are you sure you want to delete your account?') }}
+            </h2>
+
+            <p class="mt-1 text-sm text-gray-600">
+                {{ __('Once your account is deleted, all of its resources and data will also be permanently deleted. Please confirm you would like to permanently delete your account.') }}
+            </p>
+
+            <div class="mt-6 flex justify-end space-x-2">
+                <flux:modal.close>
+                    <flux:button variant="filled">{{ __('Cancel') }}</flux:button>
+                </flux:modal.close>
+
+                <flux:button variant="danger" type="submit">{{ __('Delete Account') }}</flux:button>
+            </div>
+        </form>
+    </flux:modal>
+</section>
